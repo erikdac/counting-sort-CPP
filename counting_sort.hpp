d936538322dd6b138162315840bc6d3dcc023fb4
erikdac <erikdac@kth.se>
2015-12-02 14:00:42 +0100

    The sorting should now be able to handle bigger vectors. Also began building a test framework.

--- counting_sort.hpp
+++ counting_sort.hpp
@@ -1,43 +1,47 @@
 #include <vector>
 #include <type_traits>
- 
-template <
-	typename T, 
-	typename = typename std::enable_if<std::is_integral<T>::value, T>::type
->
-void counting_sort(std::vector<T> & v) {
-	if(v.size() == 0)
-		return;
 
-	T min = v[0];
-	T max = v[0];
-	for(int i = 1; i < v.size(); ++i) {
-		if(v[i] < min)
-			min = v[i];
-		if(v[i] > max)
-			max = v[i];
+namespace erikdac {
+
+	template <
+		typename T, 
+		typename = typename std::enable_if<std::is_integral<T>::value, T>::type
+	>
+	void counting_sort(std::vector<T> & v) {
+		if(v.size() <= 1)
+			return;
+
+		T min = v[0];
+		T max = v[0];
+		for(int i = 1; i < v.size(); ++i) {
+			if(v[i] < min)
+				min = v[i];
+			if(v[i] > max)
+				max = v[i];
+		}
+
+		T * count = new T[max-min+1];
+		for(int i = 0; i <= max-min; ++i)
+			count[i] = 0;
+
+		for(int i = 0; i < v.size(); ++i)
+			++count[v[i]-min];
+
+
+		T total = 0;
+		for(int i = 0; i <= max-min+1; ++i) {
+			T oldCount = count[i];
+			count[i] = total;
+			total += oldCount;
+		}
+
+		std::vector<T> out(v.size());
+		for(int i = 0; i < out.size(); ++i) {
+			out[count[v[i]-min]] = v[i];
+			++count[v[i]-min];
+		}
+		delete [] count;
+
+		v = out;
 	}
-
-	T count[max-min+1];
-	for(int i = 0; i <= max-min; ++i)
-		count[i] = 0;
-
-	for(int i = 0; i < v.size(); ++i)
-		++count[v[i]-min];
-
-	std::vector<T> out(v.size());
-
-	T total = 0;
-	for(int i = 0; i <= max-min+1; ++i) {
-		T oldCount = count[i];
-		count[i] = total;
-		total += oldCount;
-	}
-
-	for(int i = 0; i < out.size(); ++i) {
-		out[count[v[i]-min]] = v[i];
-		++count[v[i]-min];
-	}
-
-	v = out;
 }
--- test.cpp
+++ test.cpp
@@ -1,10 +1,75 @@
+#define CATCH_CONFIG_MAIN
+
 #include <iostream>
+#include <ctime>
 #include <vector>
+#include <algorithm>
 
 #include "counting_sort.hpp"
+#include "catch.hpp"
 
-int main() {
+// ============================================================================
+
+TEST_CASE("EMPTY VECTOR") {
+
+	SECTION("SHORT") {
+		std::vector<short> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("INT") {
+		std::vector<int> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("LONG") {
+		std::vector<long> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("LONG LONG") {
+		std::vector<long long> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("UNSIGNED SHORT") {
+		std::vector<unsigned short> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("UNSIGNED INT") {
+		std::vector<unsigned int> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("UNSIGNED LONG") {
+		std::vector<unsigned long> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+
+	SECTION("UNSIGNED LONG LONG") {
+		std::vector<unsigned long long> v;
+		erikdac::counting_sort(v);
+		REQUIRE(v.size() == 0);
+	}
+}
+
+// ============================================================================
+
+#define RANGE 10
+
+TEST_CASE("TIME COMPARISSON") {
 	std::vector<int> v;
+	for(int i = -RANGE; i < RANGE; ++i) {
+		v.push_back(-i/5);
+	}
 	v.push_back(2);
 	v.push_back(4);
 	v.push_back(3);
@@ -13,10 +78,34 @@ int main() {
 	v.push_back(1);
 	v.push_back(-2);
 
-	counting_sort(v);
+	std::vector<int> first = v;
 
+	clock_t begin = clock();
+
+	erikdac::counting_sort(first);
+	
+	clock_t end = clock();
+	double counting_time = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "Counting sort: \t" << counting_time << std::endl;
+
+
+
+	std::vector<int> second = v;
+
+	begin = clock();
+
+	erikdac::counting_sort(second);
+	
+	end = clock();
+	double std_time = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "Std sort: \t" << std_time << std::endl;
+
+
+/*
 	for(int i = 0; i < v.size(); ++i) {
-		std::cout << v[i] << " ";
+		std::cout << v[i] << std::endl;;
 	}
 	std::cout << std::endl;
+*/
+
 }
